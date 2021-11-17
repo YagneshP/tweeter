26be8ee6a7a547df383d8136509ccccfbe8a7c5b
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-16 21:39:55 -0500

    Change meaningful variable name

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,10 @@ $("document").ready(function() {
   const $textTweet = $("#tweet-text");
   $textTweet.on('keyup', function(e) {
     const $tweet = $(this).val();
-    const wordCount = 140;
+    const charCount = 140;
     const $counter = $($(this).parent('form')).find('.counter');
-    const remainingWords = wordCount- $tweet.length;
-    $counter.toggleClass('over-limit',$tweet.length > wordCount);
-    $counter.text(remainingWords);
+    const remainingChar = charCount- $tweet.length;
+    $counter.toggleClass('over-limit',$tweet.length > charCount);
+    $counter.text(remainingChar);
   })
 });
